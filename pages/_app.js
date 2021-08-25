4301b879ad3121ecd5eed523802e94117158dfd9
Kunal Bagaria <kb24x7@gmail.com>
2021-08-25 18:34:20 +0530

    Updated Theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import 'nextra-theme-docs/style.css'
+import { useEffect } from 'react'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    localStorage.setItem('theme', 'dark')
+  }, [])
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>{`
+         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap');
+         * {
+           font-family: 'Montserrat', sans-serif;
+         }
+      `}</style>
+    </>
+  ) 
 }
--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/cronrobot/docs',
+  docsRepositoryBase: 'https://github.com/cronrobot/docs/blob/master',
+  titleSuffix: ' – CronRobot',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+      <span className="mr-2 font-extrabold hidden md:inline">CronRobot</span>
+      <span className="text-gray-600 font-small hidden md:inline">
+        The open task scheduling and monitoring service. 
       </span>
     </>
   ),
@@ -16,14 +16,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="CronRobot: World's leading uptime monitor" />
+      <meta name="og:description" content="CronRobot: World's leading uptime monitor" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
+      <meta name="twitter:image" content="https://images-ext-2.discordapp.net/external/eKIPzvPAfdytyE9T04luvisqPJdsV_EqWq_dXXa7vXo/https/raw.githubusercontent.com/cronrobot/cronrobot/master/www-cronrobot/assets/Design%2520Assets/3D%2520Renders/Big%2520Logo.png?width=909&height=511" />
+      <meta name="twitter:site:domain" content="docs.cronrobot.io" />
+      <meta name="twitter:url" content="https://docs.cronrobot.io" />
       <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      <meta name="og:image" content="https://images-ext-2.discordapp.net/external/eKIPzvPAfdytyE9T04luvisqPJdsV_EqWq_dXXa7vXo/https/raw.githubusercontent.com/cronrobot/cronrobot/master/www-cronrobot/assets/Design%2520Assets/3D%2520Renders/Big%2520Logo.png?width=909&height=511" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
@@ -62,6 +62,7 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © CronRobot.</>,
   unstable_faviconGlyph: '👋',
+  darkMode: true
 }
