7533036f91c072c9f4af6c832f0158a89a3d14b9
Kunal Bagaria <61944452+kb24x7@users.noreply.github.com>
2021-08-25 19:37:03 +0530

    Trying to fix failed deployment

--- theme.config.js
+++ theme.config.js
@@ -63,6 +63,5 @@ export default {
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
   footerText: <>MIT {new Date().getFullYear()} © <a href="https://cronrobot.io">CronRobot.</a></>,
-  unstable_faviconGlyph: '👋',
-  darkMode: true
+  unstable_faviconGlyph: '👋'
 }
